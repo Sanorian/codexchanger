d3841174f093e9417655344c73de7181f89dae2e
Sanorian <krajnov.sascha@yandex.ru>
2023-11-04 23:13:17 +0400

    Написал добавление поста в базу данных.
    Требуется тестирование

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
+from mysql.connector import connect, Error
+
 app = FastAPI()
 
 @app.get("/")
 def getDataAll():
     ...
-
 @app.get("/registration")
 def registration(mail: str, nickname: str, password: str):
     ...
@@ -12,3 +13,22 @@ def registration(mail: str, nickname: str, password: str):
 @app.get("/login")
 def logIn(mail: str, password: str):
     ...
+
+@app.get("/addpost")
+def addPost(userID: int, name: str, code: str, language: str, tags: str, publicationDate: str):
+    try:
+        with connect(
+            host="localhost:3306",
+            user="root",
+            password="root"
+        ) as connection:
+            with connection.cursor() as cursor:
+                getIDRequest = "SELECT ID FROM Programs"
+                cursor.execute(getIDRequest)
+                for db in cursor:
+                    id = (db[len(db)-1]["ID"]) + 1
+            request = "INSERT INTO Programs (ID, User_ID, Name, Code, Language, Tags, Publication_date, Moderator_ID, Moderation_Date) VALUES ("+id+", "+userID+", "+name+", "+code+", "+ language+", "+tags+", "+publicationDate+', "", "")'
+            with connection.cursor() as cursor:
+                cursor.execute(request)
+    except Error as e:
+        print(e)
