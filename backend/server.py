4528773f6a93fb8bca835d4b04bb6fa52dc91899
Sanorian <krajnov.sascha@yandex.ru>
2023-11-04 18:38:46 +0400

    Сделал заготовки для серверного приложения

--- backend/server.py
+++ backend/server.py
@@ -2,5 +2,13 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def root():
-    return {"name": "Paul"}
+def getDataAll():
+    ...
+
+@app.get("/registration")
+def registration(mail: str, nickname: str, password: str):
+    ...
+
+@app.get("/login")
+def logIn(mail: str, password: str):
+    ...
