7c6cc50e960438ea2a7eae5e8038546deadab83a
Sanorian <krajnov.sascha@yandex.ru>
2023-11-05 21:42:49 +0400

    Написал получение постов с условиями и пофиксил некоторые ошибки в добавлении постов

--- backend/server.py
+++ backend/server.py
@@ -3,6 +3,7 @@ from mysql.connector import connect, Error
 
 app = FastAPI()
 
+# для получения всех постов
 @app.get("/")
 def getDataAll():
     ...
@@ -26,9 +27,49 @@ def addPost(userID: int, name: str, code: str, language: str, tags: str, publica
                 getIDRequest = "SELECT ID FROM Programs"
                 cursor.execute(getIDRequest)
                 for db in cursor:
-                    id = (db[len(db)-1]["ID"]) + 1
+                    id = int(db[len(db)-1]["ID"]) + 1
             request = "INSERT INTO Programs (ID, User_ID, Name, Code, Language, Tags, Publication_date, Moderator_ID, Moderation_Date) VALUES ("+id+", "+userID+", "+name+", "+code+", "+ language+", "+tags+", "+publicationDate+', "", "")'
             with connection.cursor() as cursor:
                 cursor.execute(request)
+                return {"res": "good"}
     except Error as e:
         print(e)
+        return {"res": "bad"}
+
+@app.get("/getposts")
+def getPosts(search: str, language: str, tags: str):
+    try:
+        with connect(
+            host="localhost:3306",
+            user="root",
+            password="root"
+        ) as connection:
+            if language and search and tags:
+                request = "SELECT ID, Name, Language, Tags, Publication_date FROM programs WHERE code LIKE '%"+search+"% AND WHERE Language='"+language+"'"
+                for tag in tags.split(" "):
+                    request += " AND WHERE code LIKE '%"+tag+"%'"         
+            elif language and search:
+                request = "SELECT ID, Name, Language, Tags, Publication_date FROM programs WHERE code LIKE '%"+search+"% AND WHERE Language='"+language+"'"
+            elif language and tags:
+                request = 'SELECT ID, Name, Language, Tags, Publication_date FROM programs WHERE Language="'+language+'"'
+                for tag in tags.split(" "):
+                    request += " AND WHERE code LIKE '%"+tag+"%'"           
+            elif search and tags:
+                request = "SELECT ID, Name, Language, Tags, Publication_date FROM programs WHERE code LIKE '%"+search+"%'"
+                for tag in tags.split(" "):
+                    request += " AND WHERE code LIKE '%"+tag+"%'"
+            elif language:
+                request = 'SELECT ID, Name, Language, Tags, Publication_date FROM programs WHERE Language="'+language+'"'
+            elif search:
+                request = "SELECT ID, Name, Language, Tags, Publication_date FROM programs WHERE code LIKE '%"+search+"%'"
+            elif tags:
+                request = "SELECT ID, Name, Language, Tags, Publication_date FROM programs"
+                for tag in tags.split(" "):
+                    request += " AND WHERE code LIKE '%"+tag+"%'"           
+            with connection.cursor() as cursor:
+                cursor.execute(request)
+                for db in cursor:
+                    return db
+    except Error as e:
+        print(e)
+        return {"res": "bad"}
