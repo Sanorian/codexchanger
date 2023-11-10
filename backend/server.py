b9f80ba5abfc677e4ea5ac7d883674b5b3c817c8
Sanorian <krajnov.sascha@yandex.ru>
2023-11-10 22:38:43 +0400

    Рефакторинг
    Co-Authored-By: IDragonThunderI <116603814+IDragonThunderI@users.noreply.github.com>

--- backend/server.py
+++ backend/server.py
@@ -28,17 +28,18 @@ def registration(mail: str, nickname: str, password: str):
             with connection.cursor() as cursor:
                 getEmail = "SELECT * FROM Users WHERE Email='"+mail+"'"
                 cursor.execute(getEmail)
-                if len(cursor.fetchall())==0:
+                if len(cursor.fetchall())!=0:
                     return {"res":"bad", "reason":"mail"}
                 getName = "SELECT * FROM Users WHERE UserName='"+nickname+"'"
                 cursor.execute(getName)
-                if len(cursor.fetchall())==0:
+                if len(cursor.fetchall())!=0:
                     return {"res":"bad", "reason":"nickname"}
                 getIDRequest = "SELECT ID FROM Users"
                 cursor.execute(getIDRequest)
                 id = int(cursor.fetchall()[len(cursor.fetchall())-1]["ID"]) + 1
                 request = "INSERT INTO Users (ID, UserName, Email, Password) VALUES ("+id+", '"+nickname+"', '"+mail+"', '"+password+"')"
                 cursor.execute(request)
+                connection.commit()
                 return {"res": "good", "id": id, "password": password, "username": nickname}
     except Error as e:
         print(e)
@@ -61,8 +62,10 @@ def logIn(mail: str, password: str):
                 cursor.execute(getMail)
                 detectedUserPassword = cursor.fetchone()
                 if detectedUserPassword == None:
+                    connection.commit()
                     return {"res": "bad", "reason": "mail"}
                 if detectedUserPassword[0] == password:
+                    connection.commit()
                     return {"res": "good", "email": mail, "password": password}
                 return {"res": "bad", "reason": "password"}
     except Error as e:
@@ -79,10 +82,12 @@ def getOnePost(postid: int):
             password="lord",
             database="CodeXChanger_DB"
         ) as connection:
-            request = "SELECT ID, Name, Language, Tags, Publication_date, Code FROM Programs WHRE ID="+postid
+            request = "SELECT ID, Name, Language, Tags, Publication_date, Code FROM Programs WHERE ID='"+postid+"'"
             with connection.cursor() as cursor:
                 cursor.execute(request)
-                return cursor.fetchall()
+                postData = cursor.fetchall()
+                connection.commit()
+                return postData
     except Error as e:
         print(e)
         return {"res": "bad"}
@@ -101,8 +106,9 @@ def addPost(userID: int, name: str, code: str, language: str, tags: str, publica
                 getIDRequest = "SELECT ID FROM Programs"
                 cursor.execute(getIDRequest)
                 id = int(cursor.fetchall()[len(cursor.fetchall())-1]["ID"]) + 1
-                request = "INSERT INTO Programs (ID, User_ID, Name, Code, Language, Tags, Publication_date, Moderator_ID, Moderation_Date) VALUES ("+id+", "+userID+", '"+name+"', '"+code+"', '"+ language+"', '"+tags+"', '"+publicationdate+", '', '')"
+                request = "INSERT INTO Programs (ID, User_ID, Name, Code, Language, Tags, Publication_date, Moderator_ID, Moderation_Date) VALUES ("+id+", "+userID+", '"+name+"', '"+code+"', '"+ language+"', '"+tags+"', '"+publicationdate+"', '', '')"
                 cursor.execute(request)
+                connection.commit()
                 return {"res": "good"}
     except Error as e:
         print(e)
@@ -119,30 +125,32 @@ def getPosts(search: str | None = None, language: str | None = None, tags: str |
             database="CodeXChanger_DB"
         ) as connection:
             if language and search and tags:
-                request = "SELECT ID, Name, Language, Tags, Publication_date FROM Programs WHERE NOT Moderator_ID = '' AND WHERE NOT Moderation_date = '' AND WHERE Code LIKE '%"+search+"% AND WHERE Language='"+language+"'"
+                request = "SELECT ID, Name, Language, Tags, Publication_date FROM Programs WHERE NOT Moderator_ID = '' AND NOT Moderation_date = '' AND Code LIKE '%"+search+"% AND Language='"+language+"'"
                 for tag in tags.split(" "):
                     request += " AND WHERE Code LIKE '%"+tag+"%'"         
             elif language and search:
-                request = "SELECT ID, Name, Language, Tags, Publication_date FROM Programs WHERE NOT Moderator_ID = '' AND WHERE NOT Moderation_date = '' AND WHERE Code LIKE '%"+search+"% AND WHERE Language='"+language+"'"
+                request = "SELECT ID, Name, Language, Tags, Publication_date FROM Programs WHERE NOT Moderator_ID = '' AND NOT Moderation_date = '' AND Code LIKE '%"+search+"% AND Language='"+language+"'"
             elif language and tags:
-                request = 'SELECT ID, Name, Language, Tags, Publication_date FROM Programs WHERE NOT Moderator_ID = '' AND WHERE NOT Moderation_date = '' AND WHERE Language="'+language+'"'
+                request = 'SELECT ID, Name, Language, Tags, Publication_date FROM Programs WHERE NOT Moderator_ID = '' AND NOT Moderation_date = '' AND Language="'+language+'"'
                 for tag in tags.split(" "):
                     request += " AND WHERE Code LIKE '%"+tag+"%'"           
             elif search and tags:
-                request = "SELECT ID, Name, Language, Tags, Publication_date WHERE NOT Moderator_ID = '' AND WHERE NOT Moderation_date = '' AND FROM Programs WHERE Code LIKE '%"+search+"%'"
+                request = "SELECT ID, Name, Language, Tags, Publication_date FROM Programs WHERE NOT Moderator_ID = '' AND NOT Moderation_date = '' AND  Code LIKE '%"+search+"%'"
                 for tag in tags.split(" "):
                     request += " AND WHERE Code LIKE '%"+tag+"%'"
             elif language:
-                request = 'SELECT ID, Name, Language, Tags, Publication_date FROM Programs WHERE NOT Moderator_ID = '' AND WHERE NOT Moderation_date = '' AND WHERE Language="'+language+'"'
+                request = 'SELECT ID, Name, Language, Tags, Publication_date FROM Programs WHERE NOT Moderator_ID = '' AND NOT Moderation_date = '' AND Language="'+language+'"'
             elif search:
-                request = "SELECT ID, Name, Language, Tags, Publication_date FROM Programs WHERE NOT Moderator_ID = '' AND WHERE NOT Moderation_date = '' AND WHERE Code LIKE '%"+search+"%'"
+                request = "SELECT ID, Name, Language, Tags, Publication_date FROM Programs WHERE NOT Moderator_ID = '' AND NOT Moderation_date = '' AND Code LIKE '%"+search+"%'"
             elif tags:
-                request = "SELECT ID, Name, Language, Tags, Publication_date FROM Programs WHERE NOT Moderator_ID = '' AND WHERE NOT Moderation_date = ''"
+                request = "SELECT ID, Name, Language, Tags, Publication_date FROM Programs WHERE NOT Moderator_ID = '' AND NOT Moderation_date = ''"
                 for tag in tags.split(" "):
                     request += " AND WHERE Code LIKE '%"+tag+"%'"           
             with connection.cursor() as cursor:
                 cursor.execute(request)
-                return cursor.fetchall()
+                postsData = cursor.fetchall()
+                connection.commit()
+                return postsData
     except Error as e:
         print(e)
         return {"res": "bad"}
@@ -158,10 +166,14 @@ def adminlogin(email: str, password: str):
             password="lord",
             database="CodeXChanger_DB"
         ) as connection:
-            request = "SELECT ID FROM Admins WHERE E-mail='"+email+"' AND WHERE Password='"+password+"'"
+            request = "SELECT ID FROM Admins WHERE Email='"+email+"' AND Password='"+password+"'"
             with connection.cursor() as cursor:
                 cursor.execute(request)
-                return cursor.fetchall()
+                if len(cursor.fetchall())==1:
+                    connection.commit()
+                    return {"res": "good"}
+                connection.commit()
+                return {"res": "bad"}
     except Error as e:
         print(e)
         return {"res": "bad"}
@@ -182,6 +194,7 @@ def adminModeratePost(postid: int, adminid: int, moderationdate: str, adminpassw
             request = "UPDATE Programs SET Moderation_Date = '"+moderationdate+"', Moderation_ID="+adminid+" WHERE ID="+postid+" AND EXISTS (SELECT * FROM Admins WHERE ID="+adminid+" AND Password='"+adminpassword+"')"
             with connection.cursor() as cursor:
                 cursor.execute(request)
+                connection.commit()
                 return {"res": "good"}
     except Error as e:
         print(e)
